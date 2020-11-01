dbf3f18666a78135c1e704274f49a09fa13d68a9
Md Mohsin Siddiqui <Mdmohsinsiddiqui5@gmail.com>
2020-11-01 21:45:56 +0530

    initial commit

--- client/src/index.js
+++ client/src/index.js
@@ -50,7 +50,7 @@ class application extends React.Component {
     //const networkId = await web3.eth.net.getId()
     //const networkData = Healthcare.networks[networkId]
    
-      const contract = new web3.eth.Contract(Healthcare, "0x0baa16ddde410d64069ffb772898ee2e3ccffa64")
+      const contract = new web3.eth.Contract(Healthcare, "0x53cd4714E2F547c980684edF545f37fa344A4A30")
       this.setState({ contract })
      
 
--- contracts/Healthcare.sol
+++ contracts/Healthcare.sol
@@ -1,7 +1,4 @@
 
-
-
-
 pragma solidity >=0.4.21 <0.7.0;
 pragma experimental ABIEncoderV2;
 /**
